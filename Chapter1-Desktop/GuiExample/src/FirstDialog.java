e61298e32f59c487544c0dc5e66191acc1665199
andrew-venenga <andrew_venenga@stu.indianhills.edu>
2019-12-10 13:25:35 -0600

    Up to date verison of java projects folder 12/10/2019

--- Chapter1-Desktop/GuiExample/src/FirstDialog.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.JOptionPane;
-
-public class FirstDialog {
-        public static void main(String[] args) {
-            JOptionPane.showMessageDialog(null, "Ok Boomer", "Boomer", JOptionPane.ERROR_MESSAGE);
-        }
-    }
-
--- Chapter1-Desktop/Math/src/DataTypes.java
+++
@@ -1,7 +0,0 @@
-public class DataTypes {
-    public static void main(String[] args){
-        byte b;
-        b = 100;
-        System.out.println(b);
-    }
-}
