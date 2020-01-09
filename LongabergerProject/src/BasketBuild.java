1082a576a3cf1b44fdf7b4ea15c63d528e08e281
andrew-venenga <andrew_venenga@stu.indianhills.edu>
2020-01-09 13:51:20 -0600

    start project 1/9/2020

---
+++ LongabergerProject/src/BasketBuild.java
@@ -0,0 +1,14 @@
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class BasketBuild {
+    //declarations
+    public static String greeting;
+
+    //constructor
+    //default
+    public BasketBuild(){
+
+    }
+}
