80b1f78c3c68803a1dde4f4a4554e08d344718a9
andrew-venenga <andrew_venenga@stu.indianhills.edu>
2020-01-28 12:22:10 -0600

    idk when this was done

--- loopexample/src/loopexample.java
+++ loopexample/src/loopexample.java
@@ -6,15 +6,28 @@ pg 324 #2
 public class loopexample {
     public static Scanner scannerIn = new Scanner(System.in);
     public static void main(String[] args) {
-        int ctr = 0;
-        String name = "";
-        while (ctr < 5) {
-            display();
+        int numCtr = 0;
+        int Number = 0;
+        
+        while (Number != 999) {
+            Number = display();
+            numCtr++;
         }
+        System.out.println("Total" + numCtr);
     }
 
-    public static void display(){
-        System.out.println("This is kinda gay");
+    public static int display(){
+
+        System.out.println("Enter an even number or 999 to exit");
+        int Number = scannerIn.nextInt();
+        int input = Number;
+        if (input % 2 == 0 ) {
+            System.out.println (Number + " is an even number");
+            return Number;
+        }
+        else
+        System.out.println("This is ODD try again.");
+        return Number;
     }
 
 }
